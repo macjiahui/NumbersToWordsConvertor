5c233237370ad8438c6fcf12c21d857b88d37b2d
macjiahui <55211400+macjiahui@users.noreply.github.com>
2020-02-02 22:42:37 -0500

    first commit

---
+++ ReadNumberInWord/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ReadNumberInWord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numInputTextBox = new System.Windows.Forms.TextBox();
+            this.wordsTextBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(118, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(382, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number To Words";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numInputTextBox
+            // 
+            this.numInputTextBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numInputTextBox.Location = new System.Drawing.Point(164, 116);
+            this.numInputTextBox.MaxLength = 9;
+            this.numInputTextBox.Multiline = true;
+            this.numInputTextBox.Name = "numInputTextBox";
+            this.numInputTextBox.Size = new System.Drawing.Size(275, 35);
+            this.numInputTextBox.TabIndex = 1;
+            this.numInputTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // wordsTextBox
+            // 
+            this.wordsTextBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wordsTextBox.Location = new System.Drawing.Point(82, 250);
+            this.wordsTextBox.Multiline = true;
+            this.wordsTextBox.Name = "wordsTextBox";
+            this.wordsTextBox.ReadOnly = true;
+            this.wordsTextBox.Size = new System.Drawing.Size(439, 124);
+            this.wordsTextBox.TabIndex = 2;
+            // 
+            // convertButton
+            // 
+            this.convertButton.BackColor = System.Drawing.Color.Coral;
+            this.convertButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.convertButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.Location = new System.Drawing.Point(164, 189);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(111, 37);
+            this.convertButton.TabIndex = 3;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = false;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.LightGreen;
+            this.clearButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clearButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(328, 189);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(111, 37);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(409, 396);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "CopyRight © Kevin Mac";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(612, 431);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.wordsTextBox);
+            this.Controls.Add(this.numInputTextBox);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numInputTextBox;
+        private System.Windows.Forms.TextBox wordsTextBox;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ReadNumberInWord/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * This program convert NUMBER (length <= 9) to WORDS. 
+ * Author: Kevin Mac
+ */
+namespace ReadNumberInWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            
+            int number = int.Parse(numInputTextBox.Text);
+            int numberLength = numInputTextBox.Text.Length;
+            if (number < 0)
+            { 
+                number *= -1;
+                wordsTextBox.Text += "(Negative) ";
+            }
+            int hundredMillionth = number % 1000000000 / 100000000;
+            int tenMillionth = number % 100000000 / 10000000;
+            int millionth = number % 10000000 / 1000000;
+            int hundredThousandth = number % 1000000 / 100000;
+            int tenThousandth = number % 100000 / 10000;
+            int thousandth = number % 10000 / 1000;
+            int hundredth = number % 1000 / 100;
+            int tenth = number % 100 / 10;
+            int unit = number % 10;
+            
+
+            //------------------------Count = 9------------------------
+            if (numberLength == 9)
+            {
+                if (hundredMillionth == 1)
+                { wordsTextBox.Text += "One Hundred "; }
+                else if (hundredMillionth == 2)
+                { wordsTextBox.Text += "Two Hundred "; }
+                else if (hundredMillionth == 3)
+                { wordsTextBox.Text += "Three Hundred "; }
+                else if (hundredMillionth == 4)
+                { wordsTextBox.Text += "Four Hundred "; }
+                else if (hundredMillionth == 5)
+                { wordsTextBox.Text += "Five Hundred "; }
+                else if (hundredMillionth == 6)
+                { wordsTextBox.Text += "Six Hundred "; }
+                else if (hundredMillionth == 7)
+                { wordsTextBox.Text += "Seven Hundred "; }
+                else if (hundredMillionth == 8)
+                { wordsTextBox.Text += "Eight Hundred "; }
+                else if (hundredMillionth == 9)
+                { wordsTextBox.Text += "Nine Hundred "; }
+                numberLength--;
+                number %= 100000000;
+            }
+            //------------------------Length 8------------------------
+            if (numberLength == 8)
+            {
+                if (tenMillionth > 1)
+                { wordsTextBox.Text += "and "; }
+                if (tenMillionth == 1 && millionth == 0)
+                { wordsTextBox.Text += "Ten "; }
+                else if (tenMillionth == 2)
+                { wordsTextBox.Text += "Twenty "; }
+                else if (tenMillionth == 3)
+                { wordsTextBox.Text += "Thirty "; }
+                else if (tenMillionth == 4)
+                { wordsTextBox.Text += "Forty "; }
+                else if (tenMillionth == 5)
+                { wordsTextBox.Text += "Fifty "; }
+                else if (tenMillionth == 6)
+                { wordsTextBox.Text += "Sixty "; }
+                else if (tenMillionth == 7)
+                { wordsTextBox.Text += "Seventy "; }
+                else if (tenMillionth == 8)
+                { wordsTextBox.Text += "Eighty "; }
+                else if (tenMillionth == 9)
+                { wordsTextBox.Text += "Ninety "; }
+                if (millionth == 0 && (tenMillionth != 0 || hundredMillionth != 0))
+                { wordsTextBox.Text += "Million "; }
+                numberLength--;
+                number %= 10000000;
+
+                if (tenMillionth == 1)
+                {
+                    if (millionth == 1)
+                    { wordsTextBox.Text += "Eleven Million "; }
+                    else if (millionth == 2)
+                    { wordsTextBox.Text += "Twelve Million "; }
+                    else if (millionth == 3)
+                    { wordsTextBox.Text += "Thirteen Million "; }
+                    else if (millionth == 4)
+                    { wordsTextBox.Text += "Fourteen Million "; }
+                    else if (millionth == 5)
+                    { wordsTextBox.Text += "Fifteen Million "; }
+                    else if (millionth == 6)
+                    { wordsTextBox.Text += "Sixteen Million "; }
+                    else if (millionth == 7)
+                    { wordsTextBox.Text += "Seventeen Million "; }
+                    else if (millionth == 8)
+                    { wordsTextBox.Text += "Eighteen Million "; }
+                    else if (millionth == 9)
+                    { wordsTextBox.Text += "Nineteen Million "; }
+                    numberLength--;
+                    number %= 1000000;
+                }
+            }
+            //------------------------Length 7------------------------
+            if (numberLength == 7)
+            {
+                if (millionth == 1)
+                { wordsTextBox.Text += "One Million "; }
+                else if (millionth == 2)
+                { wordsTextBox.Text += "Two Million "; }
+                else if (millionth == 3)
+                { wordsTextBox.Text += "Three Million "; }
+                else if (millionth == 4)
+                { wordsTextBox.Text += "Four Million "; }
+                else if (millionth == 5)
+                { wordsTextBox.Text += "Five Million "; }
+                else if (millionth == 6)
+                { wordsTextBox.Text += "Six Million "; }
+                else if (millionth == 7)
+                { wordsTextBox.Text += "Seven Million "; }
+                else if (millionth == 8)
+                { wordsTextBox.Text += "Eight Million "; }
+                else if (millionth == 9)
+                { wordsTextBox.Text += "Nine Million "; }
+                numberLength--;
+                number %= 1000000;
+            }
+            //------------------------Length 6------------------------
+            if (numberLength == 6)
+            {
+                if (hundredThousandth == 1)
+                { wordsTextBox.Text += "One Hundred "; }
+                else if (hundredThousandth == 2)
+                { wordsTextBox.Text += "Two Hundred "; }
+                else if (hundredThousandth == 3)
+                { wordsTextBox.Text += "Three Hundred "; }
+                else if (hundredThousandth == 4)
+                { wordsTextBox.Text += "Four Hundred "; }
+                else if (hundredThousandth == 5)
+                { wordsTextBox.Text += "Five Hundred "; }
+                else if (hundredThousandth == 6)
+                { wordsTextBox.Text += "Six Hundred "; }
+                else if (hundredThousandth == 7)
+                { wordsTextBox.Text += "Seven Hundred "; }
+                else if (hundredThousandth == 8)
+                { wordsTextBox.Text += "Eight Hundred "; }
+                else if (hundredThousandth == 9)
+                { wordsTextBox.Text += "Nine Hundred "; }
+                numberLength--;
+                number %= 100000;
+            }
+            //---------------------------------Length 5---------------------------------
+            if (numberLength == 5)
+            {
+                if (tenThousandth > 1)
+                { wordsTextBox.Text += "and "; }
+                if (tenThousandth == 1 && thousandth == 0)
+                { wordsTextBox.Text += "Ten "; }
+                else if (tenThousandth == 2)
+                { wordsTextBox.Text += "Twenty "; }
+                else if (tenThousandth == 3)
+                { wordsTextBox.Text += "Thirty "; }
+                else if (tenThousandth == 4)
+                { wordsTextBox.Text += "Forty "; }
+                else if (tenThousandth == 5)
+                { wordsTextBox.Text += "Fifty "; }
+                else if (tenThousandth == 6)
+                { wordsTextBox.Text += "Sixty "; }
+                else if (tenThousandth == 7)
+                { wordsTextBox.Text += "Seventy "; }
+                else if (tenThousandth == 8)
+                { wordsTextBox.Text += "Eighty "; }
+                else if (tenThousandth == 9)
+                { wordsTextBox.Text += "Ninety "; }
+                if (thousandth == 0 && (tenThousandth != 0 || hundredThousandth != 0))
+                { wordsTextBox.Text += "Thousand "; }
+                numberLength--;
+                number %= 10000;
+                if (tenThousandth == 1)
+                {
+                    if (thousandth == 1)
+                    { wordsTextBox.Text += "Eleven Thousand "; }
+                    else if (thousandth == 2)
+                    { wordsTextBox.Text += "Twelve Thousand "; }
+                    else if (thousandth == 3)
+                    { wordsTextBox.Text += "Thirteen Thousand "; }
+                    else if (thousandth == 4)
+                    { wordsTextBox.Text += "Fourteen Thousand "; }
+                    else if (thousandth == 5)
+                    { wordsTextBox.Text += "Fifteen Thousand "; }
+                    else if (thousandth == 6)
+                    { wordsTextBox.Text += "Sixteen Thousand "; }
+                    else if (thousandth == 7)
+                    { wordsTextBox.Text += "Seventeen Thousand "; }
+                    else if (thousandth == 8)
+                    { wordsTextBox.Text += "Eighteen Thousand "; }
+                    else if (thousandth == 9)
+                    { wordsTextBox.Text += "Nineteen Thousand "; }
+                    numberLength--;
+                    number %= 1000;
+                }
+            }
+            //---------------------------------Length 4---------------------------------
+            if (numberLength == 4)
+            {
+                if (thousandth == 1)
+                { wordsTextBox.Text += "One Thousand "; }
+                else if (thousandth == 2)
+                { wordsTextBox.Text += "Two Thousand "; }
+                else if (thousandth == 3)
+                { wordsTextBox.Text += "Three Thousand "; }
+                else if (thousandth == 4)
+                { wordsTextBox.Text += "Four Thousand "; }
+                else if (thousandth == 5)
+                { wordsTextBox.Text += "Five Thousand "; }
+                else if (thousandth == 6)
+                { wordsTextBox.Text += "Six Thousand "; }
+                else if (thousandth == 7)
+                { wordsTextBox.Text += "Seven Thousand "; }
+                else if (thousandth == 8)
+                { wordsTextBox.Text += "Eight Thousand "; }
+                else if (thousandth == 9)
+                { wordsTextBox.Text += "Nine Thousand "; }
+                numberLength--;
+                number %= 1000;
+            }
+            //---------------------------------Length 3---------------------------------
+            if (numberLength == 3)
+            {
+                if (hundredth == 1)
+                { wordsTextBox.Text += "One hundred "; }
+                else if (hundredth == 2)
+                { wordsTextBox.Text += "Two hundred "; }
+                else if (hundredth == 3)
+                { wordsTextBox.Text += "Three hundred "; }
+                else if (hundredth == 4)
+                { wordsTextBox.Text += "Four hundred "; }
+                else if (hundredth == 5)
+                { wordsTextBox.Text += "Five hundred "; }
+                else if (hundredth == 6)
+                { wordsTextBox.Text += "Six hundred "; }
+                else if (hundredth == 7)
+                { wordsTextBox.Text += "Seven hundred "; }
+                else if (hundredth == 8)
+                { wordsTextBox.Text += "Eight hundred "; }
+                else if (hundredth == 9)
+                { wordsTextBox.Text += "Nine hundred "; }
+                numberLength--;
+                number %= 100;
+            }
+            //---------------------------------Length 2---------------------------------
+            if (numberLength == 2)
+            {
+                if ((hundredMillionth > 0 || tenMillionth > 0 || millionth > 0 || 
+                    hundredThousandth > 0 || tenThousandth > 0 || thousandth > 0 || 
+                    hundredth > 0) && unit != 0)
+                { wordsTextBox.Text += "and "; }
+                if (tenth == 1)
+                {
+                    if (unit == 0)
+                    { wordsTextBox.Text += "Ten"; }
+                    else if (unit == 1)
+                    { wordsTextBox.Text += "Eleven";}
+                    else if (unit == 2)
+                    { wordsTextBox.Text += "Twelve";}
+                    else if (unit == 3)
+                    { wordsTextBox.Text += "Thirteen";}
+                    else if (unit == 4)
+                    { wordsTextBox.Text += "Fourteen";}
+                    else if (unit == 5)
+                    { wordsTextBox.Text += "Fifteen";}
+                    else if (unit == 6)
+                    { wordsTextBox.Text += "Sixteen";}
+                    else if (unit == 7)
+                    { wordsTextBox.Text += "Seventeen";}
+                    else if (unit == 8)
+                    { wordsTextBox.Text += "Eighteen";}
+                    else if (unit == 9)
+                    { wordsTextBox.Text += "Nineteen";}
+                    return;
+                }
+                else if (tenth == 2)
+                { wordsTextBox.Text += "Twenty "; }
+                else if (tenth == 3)
+                { wordsTextBox.Text += "Thirty "; }
+                else if (tenth == 4)
+                { wordsTextBox.Text += "Forty "; }
+                else if (tenth == 5)
+                { wordsTextBox.Text += "Fifty "; }
+                else if (tenth == 6)
+                { wordsTextBox.Text += "Sixty "; }
+                else if (tenth == 7)
+                { wordsTextBox.Text += "Seventy "; }
+                else if (tenth == 8)
+                { wordsTextBox.Text += "Eightty "; }
+                else if (tenth == 9)
+                { wordsTextBox.Text += "Ninety "; }
+                numberLength--;
+            }
+            //---------------------------------Length 1---------------------------------
+            if (numberLength == 1)
+            {
+                if (unit == 1)
+                { wordsTextBox.Text += "One "; }
+                else if (unit == 2)
+                { wordsTextBox.Text += "Two "; }
+                else if (unit == 3)
+                { wordsTextBox.Text += "Three "; }
+                else if (unit == 4)
+                { wordsTextBox.Text += "Four "; }
+                else if (unit == 5)
+                { wordsTextBox.Text += "Five "; }
+                else if (unit == 6)
+                { wordsTextBox.Text += "Six "; }
+                else if (unit == 7)
+                { wordsTextBox.Text += "Seven "; }
+                else if (unit == 8)
+                { wordsTextBox.Text += "Eight "; }
+                else if (unit == 9)
+                { wordsTextBox.Text += "Nine "; }
+            }
+            convertButton.Enabled = false;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            numInputTextBox.Clear();
+            wordsTextBox.Clear();
+            convertButton.Enabled = true;
+        }
+    }
+}
